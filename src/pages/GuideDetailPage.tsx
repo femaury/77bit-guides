8f7ffb9f46362d7a0267db1a0562b0f6d14347a0
Felix Maury <felix@pianity.com>
2025-03-28 22:08:42 +0100

    Remove print button

--- src/pages/GuideDetailPage.tsx
+++ src/pages/GuideDetailPage.tsx
@@ -132,22 +132,14 @@ export function GuideDetailPage() {
             </div>
             
             <div className="self-start">
-              <div className="flex gap-2">
-                <Button variant="secondary" onClick={() => window.print()}>
+              <Link to="/">
+                <Button variant="secondary">
                   <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                   </svg>
-                  Print
+                  Back to Guides
                 </Button>
-                <Link to="/">
-                  <Button variant="secondary">
-                    <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                    </svg>
-                    Back
-                  </Button>
-                </Link>
-              </div>
+              </Link>
             </div>
           </div>
         </header>
