c6e0187e7b129b3211c951c173def1b3d4683e6d
Felix Maury <felix@pianity.com>
2025-03-29 05:18:20 +0100

    Fix button shrinking

--- src/pages/GuideDetailPage.tsx
+++ src/pages/GuideDetailPage.tsx
@@ -161,13 +161,13 @@ export function GuideDetailPage() {
               </div>
             </div>
             
-            <div className="self-start">
+            <div className="self-start whitespace-nowrap">
               <Link to="/">
-                <Button variant="secondary">
-                  <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <Button variant="secondary" className="flex-shrink-0 whitespace-nowrap">
+                  <svg className="w-4 h-4 mr-1 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                   </svg>
-                  Back to Guides
+                  <span className="whitespace-nowrap">Back to Guides</span>
                 </Button>
               </Link>
             </div>
